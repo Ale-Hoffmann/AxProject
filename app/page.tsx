cbcf3b3ab940e7e6bcef3255994ae5b8d1683ea7
Alexandre Hoffmann <alehoffmann@edu.unsinos.br>
2026-09-17 22:51:39 -0300

    Wip: infopanels development.

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import { Container, Typography } from "@mui/material";
 import AxHeader from "./components/AxHeader";
 import { newtheme } from "./theme";
 import Carousel from "./components/Carousel";
+import InfoPanels from "./components/InfoPanels";
 
 
 export default function Home() {
   return (
-    <Container disableGutters sx={{bgcolor: "secondary.main" }}>
+    <Container disableGutters sx={{bgcolor: "primary.main" }}>
       <Carousel></Carousel>
+      <InfoPanels></InfoPanels>
     </Container>
   )
 }
